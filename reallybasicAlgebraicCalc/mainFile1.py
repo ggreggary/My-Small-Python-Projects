76c2820661faa555777fda9147d955866fd8134d
Greg J-B <ggreggary@aol.com>
2021-08-07 13:43:31 -0400

    Add files via upload
    My really basic python calculator. Super manual and beginner friendly.

---
+++ reallybasicAlgebraicCalc/mainFile1.py
@@ -0,0 +1,14 @@
+import CalcModule
+
+
+result1 = CalcModule.add(2, 3)
+print(result1)
+
+result2 = CalcModule.subtract(2, 3)
+print(result2)
+
+result3 = CalcModule.multiply(2, 3)
+print(result3)
+
+result4 = CalcModule.divide(10, 2)
+print(result4)
